284344bcd6a1016825092b7ae520f436c2d650d7
SnuggleTrouble <stabjo@outlook.com>
2022-02-01 14:13:52 +0100

    iteration 6 and 7 complete. changes to some files

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,37 +1,36 @@
-const app = require("../app");
-
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
 const Celebrity = require("../models/Celebrity.model");
 
-// Create celebrity
+// Route to show all celebrities
+router.get("/", (req, res, next) => {
+  Celebrity.find()
+    .then((celebrities) => {
+      res.render("celebrities/celebrities", { celebrities });
+    })
+    .catch((error) => {
+      console.log(`There was an error when listing celebrities: ${error}`);
+    });
+});
+
+// The GET route
 router.get("/celebrities/create", (req, res, next) => {
   res.render("/celebrities/new-celebrity");
 });
 
+// The POST route
 router.post("/celebrities/create", (req, res, next) => {
   Celebrity.create(req.body)
-    .then((newCelebrity) => {
+    .then((celebrity) => {
       res.redirect("/celebrities/celebrities");
     })
     .catch((error) => {
       console.log(
-        `There was an error when creating a new celebrity: ${error}. Please try again`
+        `There was an error when adding a new celebrity: ${error}. Please try again`
       );
       res.render("/celebrities/new-celebrity");
       next(error);
     });
 });
-
-// Route to show all celebrities
-router.get("/", (req, res, next) => {
-    Celebrity.find()
-      .then((celebrities) => {
-        res.render("celebrities/celebrities", { celebrities });
-      })
-      .catch((error) => {
-        console.log(`There was an error when loading the celebrities: ${error}`);
-      });
-  });
 
 module.exports = router;
--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,6 +1,37 @@
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
+const Celebrity = require("../models/Celebrity.model");
+const Movie = require("../models/Movie.model");
 
-// all your routes here
+// Route for listing all movies
+router.get("/", (req, res, next) => {
+  Movie.find()
+    .then((movies) => {
+      res.render("movies/movies", { movies });
+    })
+    .catch((error) => {
+      console.log(`There was an error when listing movies. ${error}`);
+    });
+});
+
+// The GET route
+router.get("/create", (req, res, next) => {
+  res.render("movies/new-movie");
+});
+
+// The POST route
+router.post("/create", (req, res, next) => {
+  Movie.create(req.body)
+    .then((movie) => {
+      res.redirect("/movies");
+    })
+    .catch((error) => {
+      console.log(
+        `There was an error when adding a movie ${error}. Please Try again`
+      );
+      res.render("movies/new-movie");
+      next(error);
+    });
+});
 
 module.exports = router;
