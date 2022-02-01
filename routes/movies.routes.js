72985cdb2cd1d3d9e18c8d291826acce33eda5e8
SnuggleTrouble <stabjo@outlook.com>
2022-02-01 16:02:07 +0100

    iteration 8, 9 and 10 complete. Some parts missing or showing errors

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -15,12 +15,12 @@ router.get("/", (req, res, next) => {
 });
 
 // The GET route
-router.get("/create", (req, res, next) => {
+router.get("/movies/create", (req, res, next) => {
   res.render("movies/new-movie");
 });
 
 // The POST route
-router.post("/create", (req, res, next) => {
+router.post("/movies/create", (req, res, next) => {
   Movie.create(req.body)
     .then((movie) => {
       res.redirect("/movies");
@@ -31,6 +31,55 @@ router.post("/create", (req, res, next) => {
       );
       res.render("movies/new-movie");
       next(error);
+    });
+});
+
+// The Details route
+router.get("/movies/:id", (req, res, next) => {
+  Movie.findById(req.params.id)
+    .populate("cast")
+    .then((movie) => {
+      res.render("movies/movie-details", { movie });
+    })
+    .catch((error) => {
+      console.log(`There was an error when retrieving the movie id ${error}`);
+    });
+});
+
+// The Delete route
+router.post("/movies/:id/delete", (req, res, next) => {
+  Movie.findByIdAndRemove(req.params.id)
+    .then((movie) => {
+      res.redirect("/movies");
+    })
+    .catch((error) => {
+      console.log(`An error occurred when removing the movie ${error}`);
+    });
+});
+
+// The Edit GET route
+router.get("/movies/:id/edit", (req, res, next) => {
+  Movie.findById(req.params.id).then((movie) => {
+    Celebrity.find()
+      .then((celebrities) => {
+        res.render("movies/edit-movie", { celebrities });
+      })
+      .catch((error) => {
+        console.log(`An error occurred when editing the movie ${error}`);
+      });
+  });
+});
+
+// The Edit POST route
+router.post("/movies/:id/edit", (req, res, next) => {
+  Movie.findByIdAndUpdate(req.params.id, req.body)
+    .populate("cast")
+    .then((movie) => {
+      res.redirect("/movies");
+    })
+    .catch((error) => {
+      console.log(`An error occurred when editing the movie ${error}`);
+      res.render("movies/edit-movie");
     });
 });
 
